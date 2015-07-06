bd5b4fbd0ae9ab5438dba43238d5905cd44839b1
maxwen <max.wenigner@gmail.com>
2015-07-06 22:27:47 +0800

    cpufreq: smartmax: bring up!

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -412,6 +412,9 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
 extern struct cpufreq_governor cpufreq_gov_interactivex;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_interactivex)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
 #endif
 
 
