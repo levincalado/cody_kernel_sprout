7c600e41c340cf9053156153b33b8554cc87c3bd
thewisenerd <thewisenerd@users.noreply.github.com>
2015-11-14 04:09:20 +0530

    tmd2771: stop fetching unused als value

--- drivers/misc/mediatek/alsps/tmd2771/tmd2771.c
+++ drivers/misc/mediatek/alsps/tmd2771/tmd2771.c
@@ -1033,10 +1033,9 @@ static int TMD2771_get_ps_value(struct TMD2771_priv *obj, u16 ps)
 int tmd2771_pocket_detection_check(void)
 {
 	int ps_val;
-	int als_val;
 
 	struct TMD2771_priv *obj = TMD2771_obj;
-	
+
 	if(obj == NULL)
 	{
 		APS_DBG("[TMD2771] TMD2771_obj is NULL!");
@@ -1046,21 +1045,11 @@ int tmd2771_pocket_detection_check(void)
 	{
 		TMD2771_enable_ps(obj->client, 1);
 
-		// @agaphetos
-		// to do: msleep(1) will be replaced 
-
-		// @thewisenerd
-		// buffer pocket_mod value
-		// sensor_check will otherwise be called every time a touch is made when screen off
-		// simply add a cputime_t;
-		// if ktime_to_ms - cputime_t < 2*sec { do not prox_check }
-		// else { prox_check }
 		msleep(50);
 
 		ps_val = TMD2771_get_ps_value(obj, obj->ps);
-		als_val = TMD2771_get_als_value(obj, obj->ps);
 
-		APS_DBG("[TMD2771] %s als_val = %d, ps_val = %d\n", __func__, als_val, ps_val);
+		APS_DBG("[TMD2771] %s ps_val = %d\n", __func__, ps_val);
 
 		TMD2771_enable_ps(obj->client, 0);
 
